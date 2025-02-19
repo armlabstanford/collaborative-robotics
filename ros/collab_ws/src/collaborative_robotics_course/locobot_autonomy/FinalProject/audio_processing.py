a6f4d9e8661221979b9b407c18026a8d862b6084
jack-Zhiyuan-Ren <zr11@stanford.edu>
2025-02-18 18:54:06 -0800

    finish audio

--- ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/audio_processing.py
+++ ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/audio_processing.py
@@ -16,8 +16,8 @@ import google.generativeai as genai
 # add the current API keys to Path
 # current_dir = os.path.dirname(os.path.abspath(__file__))
 # json_key_path = os.path.join(current_dir, "APIKeys", "NaixiangKey.json")
-json_key_path = "/home/ubuntu/Desktop/ME326/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/APIKeys/NaixiangKey.json"
-test_audio_path = "/home/ubuntu/Desktop/ME326/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/recorded_audio3.wav"
+json_key_path = "/home/locobot/Naixiang/ME326FinalProject/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/APIKeys/NaixiangKey.json"
+# test_audio_path = "/home/ubuntu/Desktop/ME326/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/recorded_audio3.wav"
 
 print("Current json key path:", json_key_path)
 if os.path.exists(json_key_path):
@@ -32,29 +32,40 @@ class AudioProcess(Node):
         # give it a default node name
         super().__init__("AudioProcess")
         self.get_logger().info("AudioProcess node has been started!")
-        # self.filename = "Audioes/recorded_audio.wav"
-        self.filename = test_audio_path
+        self.filename = "recorded_audio.wav"
+        # self.filename = test_audio_path
         self.gemini_model = genai.GenerativeModel("gemini-1.5-flash")
-        self.prompt = "In the given voice transcript, identify what the object is that the user wants. Return only the object in lowercase and do not include any whitespaces, punctuation, or new. Here is the voice transcript: "
+        self.prompt = ("In the given voice transcript, identify what the object is that the user wants."
+                       "Return only the object in lowercase and do not include any whitespaces, punctuation, or new."
+                       "If all contents of voice transcript has some fruit unrelated words, just responce no objects."
+                       "If the voice transcript has the actions, please also return the action in the same pattern of objects in this format: objects, action"
+                       "Here is the voice transcript: ")
         
         self.item = ""
+        self.recording_status  = False
 
         self.audio_publisher = self.create_publisher(String, 'AudioItem', 10)
 
         self.modify_audio_subscriber = self.create_subscription(Bool, 'ModifyAudioStatus', self.modify_audio_callback, 10)
 
         self.timer_publish_audio = self.create_timer(1.0, self.audio_item_publisher)
-        # self.timer_receive_audio = self.create_timer(5.0, self.audio_input_check_callback)
+        # self.timer_receive_audio = self.create_timer(1.0, self.audio_input_check_callback)
 
     def audio_item_publisher(self):
         msg = String()
         msg.data = self.item
         self.audio_publisher.publish(msg)
-
+    
     def modify_audio_callback(self, msg):
         self.get_logger().info(f"Received ModifyAudioStatus: {msg.data}")
+        # edge detector
         if msg.data == True:
-            self.audio_process()
+            if self.recording_status == False:
+                self.recording_status = True
+                self.audio_process()
+                self.get_logger().info("Finish Recording")
+        else:
+            self.recording_status = False
 
     def audio_process(self):
         # 1. Record from microphone
@@ -73,22 +84,22 @@ class AudioProcess(Node):
         """
         print("Recording started...")
         # Record audio (mono channel)
-        # recorded_data = sd.rec(
-        #     int(duration * sample_rate),
-        #     samplerate=sample_rate,
-        #     channels=1,
-        #     dtype='int16'  # 16-bit PCM
-        # )
-        # sd.wait()  # Wait until recording is complete
-        # print("Recording complete. Saving audio...")
+        recorded_data = sd.rec(
+            int(duration * sample_rate),
+            samplerate=sample_rate,
+            channels=1,
+            dtype='int16'  # 16-bit PCM
+        )
+        sd.wait()  # Wait until recording is complete
+        print("Recording complete. Saving audio...")
 
-        # # Write the audio data to a WAV file
-        # with wave.open(filename, 'wb') as wf:
-        #     wf.setnchannels(1)                 # mono
-        #     wf.setsampwidth(2)                # 2 bytes (16 bits)
-        #     wf.setframerate(sample_rate)
-        #     wf.writeframes(recorded_data.tobytes())
-        # print(f"Audio saved to {filename}")
+        # Write the audio data to a WAV file
+        with wave.open(filename, 'wb') as wf:
+            wf.setnchannels(1)                 # mono
+            wf.setsampwidth(2)                # 2 bytes (16 bits)
+            wf.setframerate(sample_rate)
+            wf.writeframes(recorded_data.tobytes())
+        print(f"Audio saved to {filename}")
 
     def transcribe_audio(self, filename, sample_rate=16000):
         """
