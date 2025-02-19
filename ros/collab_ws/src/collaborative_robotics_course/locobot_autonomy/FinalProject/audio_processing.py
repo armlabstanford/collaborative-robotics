d0d7104dead946e7436573d2f06ee3186219abfd
ngao4 <ngao4@illinois.edu>
2025-02-18 17:09:09 -0800

    test the transcribe audio function successfully

--- ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/audio_processing.py
+++ ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/audio_processing.py
@@ -17,6 +17,8 @@ import google.generativeai as genai
 # current_dir = os.path.dirname(os.path.abspath(__file__))
 # json_key_path = os.path.join(current_dir, "APIKeys", "NaixiangKey.json")
 json_key_path = "/home/ubuntu/Desktop/ME326/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/APIKeys/NaixiangKey.json"
+test_audio_path = "/home/ubuntu/Desktop/ME326/ros/collab_ws/src/collaborative_robotics_course/locobot_autonomy/FinalProject/recorded_audio3.wav"
+
 print("Current json key path:", json_key_path)
 if os.path.exists(json_key_path):
     print("File exsits!")
@@ -30,7 +32,8 @@ class AudioProcess(Node):
         # give it a default node name
         super().__init__("AudioProcess")
         self.get_logger().info("AudioProcess node has been started!")
-        self.filename = "Audioes/recorded_audio.wav"
+        # self.filename = "Audioes/recorded_audio.wav"
+        self.filename = test_audio_path
         self.gemini_model = genai.GenerativeModel("gemini-1.5-flash")
         self.prompt = "In the given voice transcript, identify what the object is that the user wants. Return only the object in lowercase and do not include any whitespaces, punctuation, or new. Here is the voice transcript: "
         
@@ -54,7 +57,7 @@ class AudioProcess(Node):
         # 1. Record from microphone
         self.record_audio(self.filename, duration=5, sample_rate=16000)
         # 2. Transcribe the recorded audio
-        self.transcription = transcribe_audio(self.filename)
+        transcription = self.transcribe_audio(self.filename)
         # 3. process the transcription
         raw_item = self.generate_content(transcription)
         # print("Gemini raw output:", repr(raw_item)) 
@@ -66,23 +69,23 @@ class AudioProcess(Node):
         sample rate, then saves it to a WAV file.
         """
         print("Recording started...")
-        # Record audio (mono channel)
-        recorded_data = sd.rec(
-            int(duration * sample_rate),
-            samplerate=sample_rate,
-            channels=1,
-            dtype='int16'  # 16-bit PCM
-        )
-        sd.wait()  # Wait until recording is complete
-        print("Recording complete. Saving audio...")
+        # # Record audio (mono channel)
+        # recorded_data = sd.rec(
+        #     int(duration * sample_rate),
+        #     samplerate=sample_rate,
+        #     channels=1,
+        #     dtype='int16'  # 16-bit PCM
+        # )
+        # sd.wait()  # Wait until recording is complete
+        # print("Recording complete. Saving audio...")
 
-        # Write the audio data to a WAV file
-        with wave.open(filename, 'wb') as wf:
-            wf.setnchannels(1)                 # mono
-            wf.setsampwidth(2)                # 2 bytes (16 bits)
-            wf.setframerate(sample_rate)
-            wf.writeframes(recorded_data.tobytes())
-        print(f"Audio saved to {filename}")
+        # # Write the audio data to a WAV file
+        # with wave.open(filename, 'wb') as wf:
+        #     wf.setnchannels(1)                 # mono
+        #     wf.setsampwidth(2)                # 2 bytes (16 bits)
+        #     wf.setframerate(sample_rate)
+        #     wf.writeframes(recorded_data.tobytes())
+        # print(f"Audio saved to {filename}")
 
     def transcribe_audio(self, filename, sample_rate=16000):
         """
